6e2d6dfa22e7d33dd3bca3842249d7a445764fef
Ryan Erkal <ryanerkal@yahoo.ie>
2024-03-08 06:24:28 +0000

    services

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,11 +1,4 @@
-"use client";
-
-import Image from "next/image";
 import Link from "next/link";
-import ph1 from "@/public/ph1.png";
-import ph2 from "@/public/ph2.png";
-import ph3 from "@/public/ph3.png";
-import ph4 from "@/public/ph4.png";
 
 export default function Services() {
 	return (
@@ -13,11 +6,54 @@ export default function Services() {
 			<section
 				id="services"
 				className="max-w-7xl flex flex-col items-center justify-center mx-auto text-black px-8 py-24">
-				<h2 className="w-full text-left text-5xl font-bold tracking-tight mb-8">
-					Our Services
-				</h2>
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-16 max-w-7xl w-full">
-					<Link
+				<div className="grid grid-cols-1 md:grid-cols-2 max-w-7xl w-full">
+					<h2 className="w-full text-left text-5xl font-bold tracking-tight md:mb-8">
+						Our Services
+					</h2>
+					<div className="flex flex-col items-start w-full">
+						<div className="border-b w-full pb-6 group cursor-pointer">
+							<Link
+								href="/web-design"
+								className="text-4xl tracking-tight group-hover:pl-4 group-hover:text-orange-400 transition-all ease-in-out duration-300">
+								Web Design
+							</Link>
+						</div>
+						<div className="border-b w-full py-6 group cursor-pointer">
+							<Link
+								href="/web-dev"
+								className="text-4xl tracking-tight group-hover:pl-4 group-hover:text-orange-400 transition-all ease-in-out duration-300">
+								Web Development
+							</Link>
+						</div>
+						<div className="border-b w-full py-6 group cursor-pointer">
+							<Link
+								href="/ecom"
+								className="text-4xl tracking-tight group-hover:pl-4 group-hover:text-orange-400 transition-all ease-in-out duration-300">
+								eCommerce
+							</Link>
+						</div>
+						<div className="border-b w-full py-6 group cursor-pointer">
+							<Link
+								href="/seo"
+								className="text-4xl tracking-tight group-hover:pl-4 group-hover:text-orange-400 transition-all ease-in-out duration-300">
+								SEO
+							</Link>
+						</div>
+						<div className="w-full pt-6 group cursor-pointer">
+							<Link
+								href="/services"
+								className="text-4xl tracking-tight group-hover:pl-4 group-hover:text-orange-400 transition-all ease-in-out duration-300">
+								All Services
+							</Link>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+}
+
+/* <Link
 						href="/web-design"
 						className="flex flex-col items-start gap-8 w-full group">
 						<div className="w-full h-auto overflow-hidden rounded-xl">
@@ -80,16 +116,4 @@ export default function Services() {
 						<h1 className="text-xl font-bold text-left link">
 							eCommerce
 						</h1>
-					</Link>
-				</div>
-				<div className="flex items-center justify-center w-full z-10">
-					<Link href="/services">
-						<button className="btn btn-wide px-6 mt-16 bg-white border-2 border-gray-900 hover:bg-orange-400 hover:border-orange-400 hover:text-white hover:drop-shadow-lg transition-all ease-in-out duration-300 rounded-full">
-							All Services
-						</button>
-					</Link>
-				</div>
-			</section>
-		</>
-	);
-}
+					</Link> */
